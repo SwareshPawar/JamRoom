26ed96d230d01bf853f3b56156ab56d6f40c15bc
SwareshPawar <swareshpawar@gmail.com>
2025-08-29 19:28:01 +0530

    Sync

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  port: process.env.PORT || 5000,
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/jamroom',
-  nodeEnv: process.env.NODE_ENV || 'development',
-};
